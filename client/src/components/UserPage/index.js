922c5d13f9181c0816f940373cfa6ee0c20551d0
Chris Nyberg <chrisn83@gmail.com>
2019-06-27 12:00:01 -0400

    styling to userpage, added note section

--- client/src/components/UserPage/index.js
+++ client/src/components/UserPage/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./style.css";
 
 class UserPage extends React.Component {
-    render(){
+
+    /*  1. grabbing the userData
+        2. saving notes to user
+        3. "create a general patient form" loads createPatientForm component. questions to go through
+        4. mount answers onto table document-style component
+        5. return document converted into pdf
+    */
+
+    render() {
         return (
-            <h1>UserPage</h1>
+            <div className="container" id="userContainer">
+                <div className="row">
+                    <h1>User Page</h1>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button>send to form component</button>
+            </div>
         )
     }
 }
